878338265b799ab33e2d0ef2c8cee6a6cd0bf4f4
leojoli <leonardo.guns@gmail.com>
2023-11-03 10:19:54 -0300

    for Continue Break

--- src/ExemploForArray.java
+++ src/ExemploForArray.java
@@ -4,5 +4,10 @@ public class ExemploForArray {
        for (int i = 0; i < alunos.length; i++) {
                   System.out.println("O aluno "+i+" é o aluno "+ alunos[i]);
        }
+
+       for (String aluno : alunos) {
+        System.out.println("nome "+ aluno);
+       }
+       
     }
 }
